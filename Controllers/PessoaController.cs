19cedfa2972337d8e370bddd5f3aab0d002d8e2d
Wladimilson <9863709+wladimilson@users.noreply.github.com>
2020-05-01 15:21:34 -0300

    Create project

---
+++ Controllers/PessoaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MediatRSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PessoaController : ControllerBase
+    {
+
+        [HttpGet]
+        public void Get()
+        {
+        }
+    }
+}
